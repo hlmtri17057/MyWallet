1d0fd041858ecbe43630a21b99571441eb8e0db1
cuscedu <cuscedu@mac03.local>
2017-10-01 12:35:09 +0700

    Commit

---
+++ MyWallet/ThemNhom.swift
@@ -0,0 +1,53 @@
+//
+//  ThemNhom.swift
+//  MyWallet
+//
+//  Created by cuscedu on 9/30/17.
+//  Copyright © 2017 cuscedu. All rights reserved.
+//
+
+import UIKit
+
+class ThemNhom: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func BtAddNhomThu(sender: AnyObject) {
+        let PopupView = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("IdNhomThu") as! PopUPNhomThu
+        print("OK khoi tao thanh cong !")
+        
+        self.addChildViewController(PopupView)
+        PopupView.view.frame = self.view.frame
+        self.view.addSubview(PopupView.view)
+        PopupView.didMoveToParentViewController(self)
+    }
+
+    @IBAction func BtAddNhomChi(sender: AnyObject) {
+        let PopupView = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("IdNhomChi") as! PopupNhomChi
+        print("OK khoi tao thanh cong !")
+        
+        self.addChildViewController(PopupView)
+        PopupView.view.frame = self.view.frame
+        self.view.addSubview(PopupView.view)
+        PopupView.didMoveToParentViewController(self)
+    }
+}
